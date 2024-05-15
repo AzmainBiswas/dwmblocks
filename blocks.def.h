5a91ad3fde6505215350e34433d4753f4b1c9759
Azmain Biswas <azmainbiswas007@gmail.com>
2024-05-15 19:40:15 +0530

    Wed May 15 07:40:15 PM IST 2024

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"",        "/home/azmain/.dwm/bar_scripts/dwm_nettraf",	  2,	                0},
+    // {"",        "/home/azmain/.dwm/bar_scripts/dwm_nettraf",	  2,	                0},
     {"",        "/home/azmain/.dwm/bar_scripts/dwm_volume",	      0,	                10},
     {"",        "/home/azmain/.dwm/bar_scripts/dwm_brightness",	  0,	                15},
     {"",        "/home/azmain/.dwm/bar_scripts/dwm_battery",	  60,	                0},
@@ -10,5 +10,5 @@ static const Block blocks[] = {
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = "|";
 static unsigned int delimLen = 5;
