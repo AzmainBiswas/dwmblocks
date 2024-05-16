86bb995a64e36d5cbb36a791bf20f959fdeece20
Azmain Biswas <azmainbiswas007@gmail.com>
2024-05-16 17:05:12 +0530

    Thu May 16 05:05:11 PM IST 2024

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    // {"",        "/home/azmain/.dwm/bar_scripts/dwm_nettraf",	  2,	                0},
-    {"",        "/home/azmain/.dwm/bar_scripts/dwm_volume",	      0,	                10},
-    {"",        "/home/azmain/.dwm/bar_scripts/dwm_brightness",	  0,	                15},
-    {"",        "/home/azmain/.dwm/bar_scripts/dwm_battery",	  60,	                0},
-    {"",        "/home/azmain/.dwm/bar_scripts/dwm_wifi",	      2,	                4},
-	{"",        "/home/azmain/.dwm/bar_scripts/dwm_date",	     30,	                0},
+    // {"",        "/home/azmain/bin/dwmbar_scripts/dwm_nettraf",	  2,	                0},
+    {"",        "/home/azmain/bin/dwmbar_scripts/dwm_volume",	      1,	                10},
+    {"",        "/home/azmain/bin/dwmbar_scripts/dwm_brightness",	  0,	                15},
+    {"",        "/home/azmain/bin/dwmbar_scripts/dwm_battery",	  60,	                0},
+    {"",        "/home/azmain/bin/dwmbar_scripts/dwm_wifi",	      2,	                4},
+	{"",        "/home/azmain/bin/dwmbar_scripts/dwm_date",	     30,	                0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
